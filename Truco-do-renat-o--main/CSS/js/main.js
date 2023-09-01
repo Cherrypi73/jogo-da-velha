89c7c577c8ae8425d49c1b31632458032d51eb32
mariapassos23 <passosbarbosademoraesmaria@gmail.com>
2023-09-01 18:04:18 -0400

    transforms

--- Truco-do-renat-o--main/CSS/js/main.js
+++ Truco-do-renat-o--main/CSS/js/main.js
@@ -4,7 +4,20 @@ window.onload = ()=>{
     if("serviceWorker" in navigator){
         navigator.serviceWorker.register("./sw.js");
     }
+    SelectOXBtn.onclick =  () =>{
+        selectBox.classList.add("hide") 
+        container.classList.add("show")
+    }
+    SelectXBtn.onclick =  () =>{
+        selectBox.classList.add("hide")
+        container.classList.add("show")
+    }
+   
 };
+const selectBox =  document.querySelector(".select-box"),
+SelectXBtn = selectBox.querySelector(".playx"),
+SelectOXBtn = selectBox.querySelector(".playy"),
+container = document.querySelector(".container")
 
 
   
