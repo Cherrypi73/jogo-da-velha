fb85be1340d172fa009812e12c51a231622b1071
mariapassos23 <passosbarbosademoraesmaria@gmail.com>
2023-09-02 19:02:34 -0400

    git move options

--- Truco-do-renat-o--main/CSS/js/main.js
+++ Truco-do-renat-o--main/CSS/js/main.js
@@ -28,13 +28,41 @@ window.onload = ()=>{
 let playerX = "fas fa-times";
 let playerY = "far fa-circle"
 function clickedBox(element){
+    console.log(element)
  if(players.classList.contains("players")){
  element.innerHTML =`<i class="${playerX}"> </i>`
+players.classList.add("active")
  }else{
     element.innerHTML =`<i class="${playerY}"> </i>`
+    players.classList.add("active")
  }
+ element.style.pointerEvents = "none" //faz com que o elemento nao repita o valor
+ let randomTime =((Math.random() *1000) +200).toFixed();
+ setTimeout(()=>{
+    clickBot();
+ }, randomTime)
 }
-
+function clickBot(){
+    let array = []
+    for(let  i =0 ; i< allBox.length; i++ ){
+      if(allBox[i].childElementCount == 0){
+        array.push(i)
+      //  console.log(i+""+ "it´s not children")
+      }
+    }
+      let  randomBox = array[Math.floor(Math.random()*array.length)]
+      console.log(randomBox)
+      if(array.length > 0){
+        if(players.classList.contains("players")){
+allBox[randomBox].innerHTML =`<i class="${playerY}"> </i>`
+players.classList.remove("active")
+ }else{
+    allBox[randomBox].innerHTML =`<i class="${playerX}"> </i>`
+    players.classList.remove("active")
+ }
+      }
+   // console.log(array)
+}
 
 
 
