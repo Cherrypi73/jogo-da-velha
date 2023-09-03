a7bf941a1f0011cd3e4f15b67287b88fe446c591
mariapassos23 <passosbarbosademoraesmaria@gmail.com>
2023-09-02 20:37:36 -0400

    git help

--- Truco-do-renat-o--main/CSS/js/main.js
+++ Truco-do-renat-o--main/CSS/js/main.js
@@ -10,11 +10,13 @@ window.onload = ()=>{
     for (let i = 0; i < allBox.length; i++) {
         allBox[i].setAttribute("onclick", "clickedBox(this)");
      }
+    
     "use strict";
 
     if("serviceWorker" in navigator){
         navigator.serviceWorker.register("./sw.js");
     }
+}
     SelectOXBtn.onclick =  () =>{
         selectBox.classList.add("hide"), 
         container.classList.add("show"),
@@ -24,46 +26,74 @@ window.onload = ()=>{
         selectBox.classList.add("hide"),
         container.classList.add("show")    }
    
-};
+
 let playerX = "fas fa-times";
-let playerY = "far fa-circle"
+let playerY = "far fa-circle";
+let playerSignal ="o";
+runBot = true;
 function clickedBox(element){
-    console.log(element)
  if(players.classList.contains("players")){
+    playerSignal = "x"
  element.innerHTML =`<i class="${playerX}"> </i>`
-players.classList.add("active")
+players.classList.remove("active")
+element.setAttribute("id",playerSignal)
  }else{
     element.innerHTML =`<i class="${playerY}"> </i>`
     players.classList.add("active")
+    element.setAttribute("id",playerSignal)
  }
+ selectWinner()
  element.style.pointerEvents = "none" //faz com que o elemento nao repita o valor
+container.style.pointerEvents = "none";
  let randomTime =((Math.random() *1000) +200).toFixed();
  setTimeout(()=>{
     clickBot();
  }, randomTime)
 }
+
 function clickBot(){
-    let array = []
-    for(let  i =0 ; i< allBox.length; i++ ){
-      if(allBox[i].childElementCount == 0){
-        array.push(i)
-      //  console.log(i+""+ "it´s not children")
-      }
+    let array = [];
+    if(runBot){
+        playerSignal = "O";
+        for (let i = 0; i < allBox.length; i++) {
+            if(allBox[i].childElementCount == 0){
+                array.push(i);
+            }
+        }
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        if(array.length > 0){
+            if(players.classList.contains("players")){ 
+                playerSign = "X";
+                allBox[randomBox].innerHTML = `<i class="${playerY}"></i>`;
+                allBox[randomBox].setAttribute("id", playerSignal);
+                players.classList.add("active");
+            }else{
+                allBox[randomBox].innerHTML = `<i class="${playerX}"></i>`;
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id", playerSignal);
+            }
+            selectWinner();
+        }
+        allBox[randomBox].style.pointerEvents = "none";
+        container.style.pointerEvents = "auto";
+        playerSignal = "X";
     }
-      let  randomBox = array[Math.floor(Math.random()*array.length)]
-      console.log(randomBox)
-      if(array.length > 0){
-        if(players.classList.contains("players")){
-allBox[randomBox].innerHTML =`<i class="${playerY}"> </i>`
-players.classList.remove("active")
- }else{
-    allBox[randomBox].innerHTML =`<i class="${playerX}"> </i>`
-    players.classList.remove("active")
- }
-      }
-   // console.log(array)
 }
+      
 
+function getIdVal(classname){
+    return document.querySelector(".col" + classname.id)
+}
+function checkIdSign(val1, val2, val3, sign){ 
+    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
+        return true;
+    }
+}
+function selectWinner(){
+    if(checkIdSign(1,2,3,playerSignal) || checkIdSign(4,5,6, playerSignal) || checkIdSign(7,8,9, playerSignal) || checkIdSign(1,4,7, playerSignal) || checkIdSign(2,5,8, playerSignal) || checkIdSign(3,6,9, playerSignal) || checkIdSign(1,5,9, playerSignal) || checkIdSign(3,5,7, playerSignal)){
+ console.log(playerSignal + " "+" is the winner")
+    }
+    }
 
-
+ //
   
